562d629269ed3bd1a26b06d4823b3d29a89a7f71
wappon_28_dev <wappon_28_dev@ruru.be>
2023-04-09 16:57:38 +0900

    :green_heart: シークレット類追加

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { createHono } from "./lib/constant";
 const app = createHono();
 
 app.get("/", (ctx) => ctx.json({ message: "success" }));
+app.post("/", (ctx) => ctx.json({ message: "success" }));
 
 export default app;
